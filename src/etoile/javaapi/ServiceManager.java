919d08c642e03e5a2fdeeb95aaf11d876d469b9e
Rui <Rui@10.10.33.32>
2012-02-09 18:23:53 +0000

    criei um construtor que faz load do driver sql

--- src/etoile/javaapi/ServiceManager.java
+++ src/etoile/javaapi/ServiceManager.java
@@ -21,13 +21,16 @@ public class ServiceManager {
     private int user_id;
     DBConnect db;
 
+    public ServiceManager() throws SQLException, IllegalAccessException, InstantiationException, ClassNotFoundException {
+        db = new DBConnect(SQLInstruct.dbAdress, SQLInstruct.dbUsername, SQLInstruct.dbPassword);
+        db.loadDriver();
+    }
+
     public boolean setAuthentication(String username, String sha1_of_password) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
         this.username = username;
         this.sha1_of_password = sha1_of_password;
 
 
-        db = new DBConnect(SQLInstruct.dbAdress, SQLInstruct.dbUsername, SQLInstruct.dbPassword);
-        db.loadDriver();
         String sqlStatement = SQLInstruct.login(username, sha1_of_password);
         System.out.println(sqlStatement);
         ResultSet rSet = db.queryDB(sqlStatement);
